b09ae91d4743c0a887a69bba5ffae305d9e72790
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-20 14:29:09 +0700

    css stuff to make it legible

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ var fizzBuzz = "fizzbuzz!!";
 
 for (var i = 0; i < 101; i++) {
 	if (i % 3 === 0){
-		$("p").append("<span>" + fizz + "</span>");
+		$("p").append("<span class=\"f\">" + fizz + "</span>");
 
 	}
 	else if (i % 5 === 0) {
-		$("p").append("<span>" + buzz + "</span>");
+		$("p").append("<span class=\"b\">" + buzz + "</span>");
 	}
 
 	else if (i % 15 === 0) {
-		$("p").append("<span>" + fizzBuzz + "</span>");
+		$("p").append("<span class=\"fb\">" + fizzBuzz + "</span>");
 	}
 	else {
 		$("p").append("<span>" + i + "</span>");
