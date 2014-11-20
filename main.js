0f963afa3d233d260d10a65745bafbd1f9add657
jocelynalsdorf <jfrodsla@gmail.com>
2014-11-20 14:31:44 +0700

    solved 15% problem by moving to first if statement to override the else if's of 3 and 5

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ var fizzBuzz = "fizzbuzz!!";
 
 
 for (var i = 0; i < 101; i++) {
-	if (i % 3 === 0){
+	
+	if (i % 15 === 0) {
+		$("p").append("<span class=\"fb\">" + fizzBuzz + "</span>");
+	}
+	else if (i % 3 === 0){
 		$("p").append("<span class=\"f\">" + fizz + "</span>");
 
 	}
@@ -15,9 +19,7 @@ for (var i = 0; i < 101; i++) {
 		$("p").append("<span class=\"b\">" + buzz + "</span>");
 	}
 
-	else if (i % 15 === 0) {
-		$("p").append("<span class=\"fb\">" + fizzBuzz + "</span>");
-	}
+	
 	else {
 		$("p").append("<span>" + i + "</span>");
 	}
